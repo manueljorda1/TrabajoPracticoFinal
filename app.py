c6a37b196b815f5fc0cfa56dbcb8b52871f8c9a1
ManuelJorda <manueljorda79@gmail.com>
2024-07-29 10:46:16 -0300

    arreglo detalles

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
 class Producto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
